ea696416d1bdc7448463e6e157b4049ae3ec3da1
Becsibalint <36741931+Becsibalint@users.noreply.github.com>
2018-05-10 20:21:12 +0200

    Add files via upload

---
+++ feladat3C.py
@@ -0,0 +1,24 @@
+#primszámok 10001
+
+def primszamok():
+    list=[]
+    f=0
+
+
+    while len(list)<10001:
+
+        db=0
+        for i in range(1,f+1):
+            if f%i==0:
+                db+=1
+        if db==2:
+            list.append(i)
+        f+=1
+
+    return list
+
+
+
+
+
+print(primszamok()[10000])
